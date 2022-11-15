1f0c59139c7c098665eafd5459215f09d90cc59a
Mephistophilian <43144386+Mephistophilian@users.noreply.github.com>
2022-11-14 19:55:32 -0500

    replaced DodgeBallCollide with DodgeballController
    controller SHOULD IN THEORY:
    - send a player to the outzone when hit with a ball (that didnt hit the ground first).
    - catching the ball saves the player and gets the thrower out

---
+++ Assets/DodgeballCode/DodgeballController.cs
@@ -0,0 +1,66 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class DodgeballController : UdonSharpBehaviour
+{
+    private bool hitFloor = true;               
+    private bool hasEliminatedPlayer = false;   //ensures each throw can only knock out one person
+    private VRC_Pickup pickup;
+
+    public bool wasHit = false;
+    public VRCPlayerApi Thrower;
+    private VRCPlayerApi playerHit;
+
+    public Vector3 OutZonePosition; //TeamOnePosition, TeamTwoPosition;
+    public Quaternion OutZoneRotation; //TeamOneRotation, TeamTwoRotation;
+
+    private void Start()
+    {
+        pickup = this.gameObject.GetComponent<VRC_Pickup>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "floor")
+        {
+            hitFloor = true;
+            hasEliminatedPlayer = false;
+        }
+    }
+
+    public override void OnPlayerCollisionEnter(VRCPlayerApi player)
+    {
+        if (!hitFloor && !hasEliminatedPlayer)
+        {
+            playerHit = player;
+            wasHit = true;
+        }
+    }
+
+    public override void OnPlayerCollisionExit(VRCPlayerApi player)
+    {
+        if(wasHit)
+        {
+            playerHit.TeleportTo(OutZonePosition, OutZoneRotation);
+            hasEliminatedPlayer = true;
+        }
+    }
+
+    public override void OnPickup()
+    {
+        if(hitFloor)
+        {
+            hitFloor = false;
+            Thrower = pickup.currentPlayer;
+        }
+        else
+        {
+            if (wasHit)
+                wasHit = false;
+            Thrower.TeleportTo(OutZonePosition,OutZoneRotation);
+            Thrower = pickup.currentPlayer;
+        }
+    }
+}
