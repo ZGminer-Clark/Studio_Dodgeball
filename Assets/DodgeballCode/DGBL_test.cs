3c67f5c71c5b59f69a8e58e2a656ec22b4cda49f
ZGminer <ZGminer@users.noreply.github.com>
2022-12-01 20:57:36 -0500

    made some test code slash graphs

---
+++ Assets/DodgeballCode/DGBL_test.cs
@@ -0,0 +1,32 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class DGBL_test : UdonSharpBehaviour
+{
+    private SphereCollider colliderSphere;
+
+    public Transform outZone;
+
+
+    // disable collider on pickup and reenable on drop
+    public override void OnPickup()
+    {
+        base.OnPickup();
+        {
+            colliderSphere.enabled = !colliderSphere.enabled;
+        }
+    }
+    public override void OnDrop()
+    {
+        base.OnDrop();
+        {
+            colliderSphere.enabled = !colliderSphere.enabled;
+        }
+    }
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+
+    }
+}
