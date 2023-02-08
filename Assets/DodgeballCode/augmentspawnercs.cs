cdb61664cd86d7ef9bc384cef07a001e31114e4a
Zachary Andre <zaxatax2@gmail.com>
2023-02-08 18:19:13 -0500

    Improved augmentspawner script logic

--- Assets/DodgeballCode/augmentspawnercs.cs
+++ Assets/DodgeballCode/augmentspawnercs.cs
@@ -1,35 +1,72 @@
-﻿
-using UdonSharp;
+﻿using UdonSharp;
 using System.Collections;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 
-public class augmentspawnercs : UdonSharpBehaviour
+public class AugmentSpawner : UdonSharpBehaviour
 {
-    public GameObject upgradePrefabs;
+    public GameObject[] upgradePrefabs;
     public float upgradeRespawnTime = 15.0f;
     private bool isHoldingUpgrade;
-    
-    //Allows player to pick up upgrade if they don't already have one
-    private void OnPlayerTriggerEnter(VRCPlayerApi player) {
-        if (!isHoldingUpgrade) {
-            upgradePrefabs.SetActive(false);
-            SendCustomEventDelayedSeconds("upgradeRespawn", upgradeRespawnTime);
-            //TODO: Implement upgrade
+
+    // Allows player to pick up upgrade if they don't already have one
+    private void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        if (!isHoldingUpgrade)
+        {
+            int upgradeIndex = Random.Range(0, upgradePrefabs.Length);
+            upgradePrefabs[upgradeIndex].SetActive(false);
+            SendCustomEvent("ApplyUpgrade", upgradeIndex);
+            SendCustomEventDelayedSeconds("UpgradeRespawn", upgradeRespawnTime, upgradeIndex);
             Debug.Log("Upgrade picked up");
         }
     }
 
-    //Respawns prefab after "upgradeRespawnTime" seconds of picking it up
-    private void upgradeRespawn() {
-        upgradePrefabs.SetActive(true);
+    // Respawns prefab after "upgradeRespawnTime" seconds of picking it up
+    private void UpgradeRespawn(int upgradeIndex)
+    {
+        upgradePrefabs[upgradeIndex].SetActive(true);
     }
 
-    //Spawns the prefabs for the first time *Random Selection Required*
-    private void Start() {
-        upgradeRespawn();
-    } 
-}
+    // Spawns the prefabs for the first time
+    private void Start()
+    {
+        for (int i = 0; i < upgradePrefabs.Length; i++)
+        {
+            upgradePrefabs[i].SetActive(false);
+        }
+        int upgradeIndex = Random.Range(0, upgradePrefabs.Length);
+        upgradePrefabs[upgradeIndex].SetActive(true);
+    }
 
-    //have delay before powerups spawn
+    // Apply the upgrade
+    private void ApplyUpgrade(int upgradeIndex)
+    {
+        switch (upgradeIndex)
+        {
+            case 0:
+                // Apply Upgrade 1
+                /* Ex:
+                 Multiball multiball = gameObject.GetComponent<Multiball>();
+              if (multiball != null)
+              {
+                multiball.Activate();
+              }
+              else
+              {
+                Debug.LogError("Multiball script not found");
+              } */
+                break;
+            case 1:
+                // Apply Upgrade 2
+                break;
+            case 2:
+                // Apply Upgrade 3
+                break;
+            default:
+                Debug.LogError("Invalid upgrade index!");
+                break;
+        }
+    }
+}
